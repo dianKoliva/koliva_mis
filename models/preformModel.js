4fa209529d91c1525d21583d04b021661c3c4a32
dianKoliva <dianpretty01@gmail.com>
2021-10-29 15:07:27 +0200

    new changes

---
+++ models/preformModel.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Preform = new Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    plate_no: { type: String, required: true },
+    type: { type: String, required: true },
+    brand: { type: String, required: true },
+    chassis_no: { type: String, required: true },
+
+    chassis_quantity:{ type: Number, required: true },
+    screw_quantity:{ type: Number, required: true },
+    baguette_quantity:{ type: Number, required: true },
+    disk_quantity:{ type: Number, required: true },
+    paint_quantity:{ type: Number, required: true },
+    ressort_quantity:{ type: Number, required: true },
+
+    chassis_up:{ type:Number, required: true },
+    screw_up:{ type: Number, required: true },
+    baguette_up:{ type: Number, required: true },
+    disk_up:{ type: Number, required: true },
+    paint_up:{ type: Number, required: true },
+    ressort_up:{ type: Number, required: true },
+})
+
+var Preform=mongoose.model('Preform',Preform);
+module.exports=Preform;
