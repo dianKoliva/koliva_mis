4c311773f147c4c3480b227f1539873e23976a93
dianKoliva <dianpretty01@gmail.com>
2021-11-02 06:14:06 +0200

    new changes

--- routes/preformRoutes.js
+++ routes/preformRoutes.js
@@ -8,7 +8,7 @@ const formControl=require("../controllers/preform.js")
 const checker=require("../middle/check");
 
 router.get("/",checker,formControl.getAll);
-router.post("/",checker,formControl.saveForm)
+router.post("/register",checker,formControl.saveForm)
 router.get("/:id",checker,formControl.getById)
 
 router.put("/:id",checker,formControl.update);
