90f77b83a28af2ebdb991510f780eeaa08055785
dianKoliva <dianpretty01@gmail.com>
2021-11-04 15:54:04 +0200

    new changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,22 +2,14 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/userModel');
 
-
 const userControl=require("../controllers/user.js")
-
 const checker=require("../middle/check")
-
-
 router.post('/signup',userControl.signUp )
 
 router.post("/login",userControl.login)
-
 
 router.get('/',checker,userControl.getAllUsers )
 router.get('/:id',checker, userControl.getById)
 router.put("/:id",checker,userControl.update)
 router.delete("/:id",checker,userControl.deleteById);
-
-
-
 module.exports = router;
